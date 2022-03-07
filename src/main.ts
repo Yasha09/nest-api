4334daa5193754771faeb074e1a52cd53e8f1899
yasha <ysahakyan1998@gmail.com>
2022-03-07 10:21:19 +0400

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   const options = new DocumentBuilder()
     .setTitle('Nest Api')
     .setDescription('Nest api with signup, signin, CRUD ')
